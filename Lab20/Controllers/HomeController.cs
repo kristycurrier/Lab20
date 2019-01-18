9f5c890cf502c8959b33ad3926467c988535563f
Kristy Currier <kristy.currier@gmail.com>
2019-01-18 11:48:13 -0500

    The index page is partially complete

--- Lab20/Controllers/HomeController.cs
+++ Lab20/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Lab20.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Kristy Currier";
 
             return View();
         }
